bb72e4cc41951a49c4c7d14d1640c3b74fcb849b
root <cksdnd004@naver.com>
2024-07-05 00:20:40 +0900

    vuetify playground progress

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,6 @@ import { storeToRefs } from "pinia";
 
 import { NAV_LIST } from "@/router/index.ts";
 import { useUserStore } from "@/store/userStore";
-import { is } from "@babel/types";
 
 const route = useRoute();
 const router = useRouter();
@@ -42,23 +41,6 @@ watch(
 
   <p><strong>Current route path:</strong> {{ route.fullPath }}</p>
   <nav>
-    <!-- <v-navigation-drawer>
-      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
-      <v-divider></v-divider>
-      <template v-for="navItem in NAV_LIST">
-        <v-list-item
-          link
-          :to="navItem.path"
-          v-if="
-            !!isLoggedIn
-              ? navItem.meta.requiresAuth
-              : !navItem.meta.requiresAuth
-          "
-          >{{ navItem.label }}</v-list-item
-        >
-      </template>
-    </v-navigation-drawer> -->
-
     <ul>
       <template v-for="navItem in NAV_LIST">
         <li
