e03d09a99aa687b21401e109d2bb810bc082566a
root <cksdnd004@naver.com>
2024-08-07 19:05:02 +0900

    pm2 script test

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-npm install
+# npm install
 # pm2 start dist
 # pm2 start npm --name "hkqa-server" -- run "start:prod"
 pm2 start ./ecosystem.config.js 
