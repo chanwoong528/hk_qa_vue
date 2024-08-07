40490ecbfd1bfee18f9105ac4abbbbd23ccedfd1
문찬웅 <mooncw@hankookilbo.com>
2024-08-07 16:54:59 +0900

    pm2 script done

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,11 +2,11 @@
 REPOSITORY=/var/www
 
 cd $REPOSITORY
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# npm install
+npm install
 # pm2 start dist
 # pm2 start npm --name "hkqa-server" -- run "start:prod"
 pm2 start ./ecosystem.config.js 
