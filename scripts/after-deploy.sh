e09c08b55408f96ae90760b5e550473a12bac0b0
문찬웅 <mooncw@hankookilbo.com>
2024-08-07 09:18:52 +0900

    action yml done

---
+++ scripts/after-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+REPOSITORY=/home/ubuntu/vue
+
+cd $REPOSITORY
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# npm install
+# pm2 start dist
+# pm2 start npm --name "hkqa-server" -- run "start:prod"
