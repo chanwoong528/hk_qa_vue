e7887265aca255ec5e307834dcd66dbe11439cc7
root <cksdnd004@naver.com>
2024-08-07 19:13:53 +0900

    pm2 script test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         PM2_SERVE_PATH: '.',
         PM2_SERVE_PORT: 3001,
         PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: 'dist/'
+        PM2_SERVE_HOMEPAGE: './dist/'
       }
     }
   ]
