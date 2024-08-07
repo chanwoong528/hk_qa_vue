6268cc457f12e6872ace22fd7ff2d7cbd5228336
root <cksdnd004@naver.com>
2024-08-07 19:10:37 +0900

    pm2 script test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,14 @@ module.exports = {
     {
       name: "hkqa-client", // 실행할 앱의 이름
       script: 'serve',
-      args: 'dist/ 3001 --spa',
       watch: true, // 파일 변경 모니터링, 파일 변동시 재시작
       exec_mode: "cluster",
       env: {
-        "NODE_ENV": "prod"
+        NODE_ENV: "prod",
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3001,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: 'dist/'
       }
     }
   ]
