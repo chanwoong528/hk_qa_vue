f3d78773fa56f74f3bb3bbf8e7a4c92220b42996
hankookilbo <mooncw@hankookilbo.com>
2024-09-11 11:04:15 +0900

    login progress

---
+++ src/entity/User.ts
@@ -0,0 +1,17 @@
+export default class UserClass {
+  // id: string;
+  // username: string;
+  // role: E_Role;
+  // email: string;
+  // userStatus: E_UserStatus;
+  // isPwDefault?: boolean;
+  // createdAt?: string;
+  // access_token?: string;
+  // refresh_token?: string;
+
+  public id = "";
+  public email = "";
+  public pw = "";
+
+  constructor() {}
+}
