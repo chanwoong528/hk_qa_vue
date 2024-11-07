89fbec3117427f1821d9225343b7362bd8cb6a98
mooncw@hankookilbo.com <mooncw@hankookilbo.com>
2024-11-07 09:43:37 +0900

    board comment hiding admin name fixed

--- src/components/list/CommentItem.vue
+++ src/components/list/CommentItem.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { E_EditorType, E_ReactionParentType, E_ReactionType, E_Role } from "@/types/enum.d";
 import type { IComment } from "@/types/types";
-import { ref, defineProps, } from "vue";
+import { ref, defineProps } from "vue";
 
 import CommentReactionList from "./CommentReactionList.vue";
 
@@ -12,10 +12,7 @@ import { useUserStore } from "@/store/userStore";
 import { storeToRefs } from "pinia";
 import RichEditor from "../RichEditor.vue";
 
-
 const openChildComments = defineModel<boolean>();
-
-
 
 const props = defineProps({
   comment: {
@@ -30,9 +27,10 @@ const mouseOver = ref<boolean>(false);
 
 const isEditorFocused = ref<boolean>(false);
 
-
 const openEditorForReply = ref<boolean>(false);
 const openEditorForEdit = ref<boolean>(false);
+
+console.log(props.comment);
 
 // const commentStatus = ref<string>("") // 수정 | 답글작성 | 답글보기 ;
 
@@ -42,7 +40,13 @@ const reCommentVal = ref<string>("");
 const userStore = useUserStore();
 const { loggedInUser } = storeToRefs(userStore);
 
-const emit = defineEmits(["onSubmitComment", "onFetchCommentsBySwVersionId", "onSubmitEditComment", "onClickDeleteComment", "toggleChildComments"]);
+const emit = defineEmits([
+  "onSubmitComment",
+  "onFetchCommentsBySwVersionId",
+  "onSubmitEditComment",
+  "onClickDeleteComment",
+  "toggleChildComments",
+]);
 
 const onBlurEditorCon = (isFocusOut: boolean) => {
   if (!!isFocusOut && isEditorFocused.value) {
@@ -73,20 +77,19 @@ const onClickEdit = () => {
     reCommentVal.value = props.comment.content;
     openEditorForEdit.value = !openEditorForEdit.value;
   }
-}
+};
 const onClickDeleteComment = () => {
   if (!!props.comment?.commentId) {
     return emit("onClickDeleteComment", props.comment.commentId);
   }
-
-}
+};
 
 const onSubmitEditComment = () => {
   openEditorForEdit.value = false;
   if (!!curCommentId.value) {
     emit("onSubmitEditComment", curCommentId.value, reCommentVal.value);
   }
-}
+};
 
 const onFetchCommentsBySwVersionId = () => {
   emit("onFetchCommentsBySwVersionId");
@@ -97,14 +100,18 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
 
   reactionApi
     .POST_reaction(E_ReactionParentType.comment, btnType, parentId ? parentId : props.comment.commentId)
-    .then((res) => {
+    .then(res => {
       mouseOver.value = false;
       return onFetchCommentsBySwVersionId();
     });
 };
 
+const renderCommentUserName = () => {
+  if (!!props.hideAdmin && loggedInUser?.value?.role === E_Role.tester && props.comment?.user.role !== E_Role.tester)
+    return "admin";
 
-
+  return props.comment?.user.username;
+};
 </script>
 
 <template>
@@ -114,7 +121,9 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
     <v-card class="mx-auto comment-card" :variant="child ? 'tonal' : 'outlined'">
       <template v-slot:title>
         <p class="comment-title" @mouseover="mouseOver = true">
-          {{ !!hideAdmin && loggedInUser?.role === E_Role.tester ? "admin" : props.comment?.user.username }}
+          {{ renderCommentUserName() }}
+
+          <!-- {{ !!hideAdmin && loggedInUser?.role === E_Role.tester ? "admin" : props.comment?.user.username }} -->
           <span class="date" v-if="props.comment?.createdAt">{{ formatDateTime(props.comment?.createdAt) }}</span>
         </p>
       </template>
@@ -124,45 +133,59 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
 
       <ul v-if="props.comment?.reactions && !openEditorForEdit" class="reactions-con">
         <li v-for="reactionKey in Object.keys(props.comment.counts as object)">
-
-          <v-chip link v-if="props.comment?.counts && !!props.comment?.counts"
-            @click="onClickReactionBtn(reactionKey as E_ReactionType, props.comment.commentId as string)">
-            <v-icon :icon="renderIconForReaction(reactionKey as E_ReactionType).icon"
-              :color="renderIconForReaction(reactionKey as E_ReactionType).color">
+          <v-chip
+            link
+            v-if="props.comment?.counts && !!props.comment?.counts"
+            @click="onClickReactionBtn(reactionKey as E_ReactionType, props.comment.commentId as string)"
+          >
+            <v-icon
+              :icon="renderIconForReaction(reactionKey as E_ReactionType).icon"
+              :color="renderIconForReaction(reactionKey as E_ReactionType).color"
+            >
             </v-icon>
             {{ props.comment.counts[reactionKey as E_ReactionType] }}
 
-
             <v-tooltip activator="parent" location="end" max-width="300">
-              <p v-for="person in props.comment.reactions.filter((reaction) => reaction.reactionType === reactionKey)">
+              <p v-for="person in props.comment.reactions.filter(reaction => reaction.reactionType === reactionKey)">
                 {{ person.user.username }}
               </p>
             </v-tooltip>
           </v-chip>
-
         </li>
         <li v-if="!props.child" class="comment-btn-bottom">
           <div class="comment-btn-con">
-            <v-btn v-if="!!props.comment?.childComments && props.comment?.childComments.length > 0"
-              @click="() => emit('toggleChildComments', comment?.commentId)" variant="plain">
+            <v-btn
+              v-if="!!props.comment?.childComments && props.comment?.childComments.length > 0"
+              @click="() => emit('toggleChildComments', comment?.commentId)"
+              variant="plain"
+            >
               <v-icon :icon="openChildComments ? 'mdi-menu-up-outline' : 'mdi-menu-down-outline'" prepend></v-icon>
               답글 {{ comment?.childComments.length }}개
-
             </v-btn>
 
             <div class="comment-btn-control" v-if="loggedInUser?.id === props.comment?.user.id">
-
               <v-btn class="reply-btn" v-if="!openEditorForEdit" variant="plain" color="primary" @click="onClickEdit">
                 <v-icon icon="mdi-pencil" prepend></v-icon>
                 수정
               </v-btn>
-              <v-btn class="reply-btn" v-if="openEditorForEdit" variant="plain" color="success" @click="onSubmitEditComment">
+              <v-btn
+                class="reply-btn"
+                v-if="openEditorForEdit"
+                variant="plain"
+                color="success"
+                @click="onSubmitEditComment"
+              >
                 <v-icon icon="mdi-content-save-outline" prepend></v-icon>
                 저장
               </v-btn>
 
-
-              <v-btn class=" reply-btn" v-if="!openEditorForEdit" variant="plain" color="error" @click="onClickDeleteComment">
+              <v-btn
+                class="reply-btn"
+                v-if="!openEditorForEdit"
+                variant="plain"
+                color="error"
+                @click="onClickDeleteComment"
+              >
                 <v-icon icon="mdi-delete" prepend></v-icon>
                 삭제
               </v-btn>
@@ -170,8 +193,6 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
                 <v-icon icon="mdi-cancel" prepend></v-icon>
                 취소
               </v-btn>
-
-
             </div>
           </div>
 
@@ -181,20 +202,20 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
           </v-chip>
         </li>
       </ul>
-
     </v-card>
 
-    
-
     <div v-if="openEditorForReply" class="child-comment-con" @click="onFocusEditorCon(true)">
-      <RichEditor :editorType="E_EditorType.comment" v-model="reCommentVal" :isEditorFocused="isEditorFocused"
-        @onBlurEditorCon="onBlurEditorCon" />
+      <RichEditor
+        :editorType="E_EditorType.comment"
+        v-model="reCommentVal"
+        :isEditorFocused="isEditorFocused"
+        @onBlurEditorCon="onBlurEditorCon"
+      />
       <div class="reply-btn-con">
         <v-btn @click="openEditorForReply = false" variant="outlined" color="warning">취소</v-btn>
         <v-btn @click="onSubmitReComment">답글</v-btn>
       </div>
     </div>
-
   </v-list-item>
 </template>
 
@@ -222,7 +243,7 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
 .comment-item {
   position: relative;
   padding-left: 0 !important;
-  padding-top:0;
+  padding-top: 0;
 
   .comment-card {
     padding-bottom: 10px;
@@ -264,7 +285,6 @@ const onClickReactionBtn = (btnType: E_ReactionType, parentId?: string) => {
   font-weight: 500;
 
   .date {
-
     font-size: 13px;
     color: grey;
     margin-left: 10px;
